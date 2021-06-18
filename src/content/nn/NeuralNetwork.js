68f5345e906b31bf2b974b0f23305138d27a715b
Will Yelton <willhyelton@gmail.com>
2021-06-18 12:01:04 -0400

    Basic click detection for nodes and edges

--- src/content/nn/NeuralNetwork.js
+++ src/content/nn/NeuralNetwork.js
@@ -2,6 +2,8 @@ import React from "react";
 import './NeuralNetwork.css';
 import '../Page.css';
 
+const node_radius = 20;
+
 class NeuralNetwork extends React.Component {
     constructor(props) {
         super(props);
@@ -22,11 +24,83 @@ class NeuralNetwork extends React.Component {
         }
 
         this.canvasRef = React.createRef();
+
+        this.handleCanvasClick = this.handleCanvasClick.bind(this);
     }
 
     componentDidMount() {
         this.ctx = this.canvasRef.current.getContext('2d');
         this.drawNeuralNetwork();
+    }
+
+    distance(x1, y1, x2, y2) {
+        return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+    }
+
+    // distance of point (px, py) from the line that passes through the points (x1, y1) and (x2, y2).
+    distanceToLine(x1, y1, x2, y2, px, py) {
+        return Math.abs((x2 - x1) * (y1 - py) - (x1 - px) * (y2 - y1)) / (Math.sqrt((x2 - x2) ** 2 + (y2 - y1) ** 2));
+    }
+
+    // got this from https://www.geeksforgeeks.org/how-to-get-the-coordinates-of-a-mouse-click-on-a-canvas-element/
+    getMousePosition(canvas, event) {
+        let rect = canvas.getBoundingClientRect();
+        let x = event.clientX - rect.left;
+        let y = event.clientY - rect.top;
+        return [x, y];
+    }
+
+    handleCanvasClick(e) {
+        const clickPos = this.getMousePosition(this.canvasRef.current, e);
+        const nodeClicked = this.detectClickNode(clickPos[0], clickPos[1]);
+        if (nodeClicked !== -1) {
+            this.handleClickNode(nodeClicked);
+        }
+
+        const edgeClicked = this.detectClickEdge(clickPos[0], clickPos[1]);
+        if (edgeClicked !== -1) {
+            this.handleClickEdge(edgeClicked);
+        }
+    }
+
+    detectClickNode(x, y) {
+        const nodes = this.state.nn.nodes;
+
+        for (let i = 0; i < nodes.length; i++) {
+            const node = nodes[i];
+
+            if (this.distance(node.x, node.y, x, y) <= node_radius) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    detectClickEdge(x, y) {
+        const edges = this.state.nn.edges;
+        const nodes = this.state.nn.nodes;
+
+        for (let i = 0; i < edges.length; i++) {
+            const edge = edges[i];
+            const n1 = nodes[edge.n1];
+            const n2 = nodes[edge.n2];
+
+            if (((n1.x < x && x < n2.x) || (n2.x < x && x < n1.x))
+                && ((n1.y < y && y < n2.y) || (n2.y < y && y < n1.y))
+                && this.distanceToLine(n1.x, n1.y, n2.x, n2.y, x, y) <= edge.w * 10) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    handleClickNode(node) {
+        console.log('clicked node id: ' + node)
+    }
+
+    handleClickEdge(edge) {
+        console.log('clicked edge id: ' + edge)
     }
 
 
@@ -57,7 +131,7 @@ class NeuralNetwork extends React.Component {
 
             this.ctx.beginPath();
             this.ctx.fillStyle = 'purple';
-            this.ctx.arc(node.x, node.y, 20, 0, Math.PI * 2);
+            this.ctx.arc(node.x, node.y, node_radius, 0, Math.PI * 2);
             this.ctx.fill();
         }
 
@@ -74,7 +148,7 @@ class NeuralNetwork extends React.Component {
 
                 </div>
                 <div className="row nn-canvas-container">
-                    <canvas width={this.state.width} height={this.state.height} ref={this.canvasRef}/>
+                    <canvas width={this.state.width} height={this.state.height} ref={this.canvasRef} onClick={this.handleCanvasClick}/>
                 </div>
             </div>
         );
